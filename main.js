ced312ddaf7e98065f69882592b78d60dba0cf61
Kevin He <khe@ztech.us>
2020-08-31 09:57:58 -0500

    init checkin

---
+++ main.js
@@ -0,0 +1,18 @@
+import { createElement, Component, render } from "./toy-react";
+
+class MyComponent extends Component {
+    render() {
+        return <div>
+            <h1>my component</h1>
+            {this.children}
+        </div>
+    }
+}
+
+
+
+render(<MyComponent id="a" class="c" >
+    <div><p>tan</p></div>
+    <div>abc</div>
+    <div></div>
+</MyComponent>, document.body);
